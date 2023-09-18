4f1513768e9490139bb02deab6a2bfd8bc11590c
Ion Obreja <jo@da.md>
2023-09-18 14:33:38 +0300

    Base changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,38 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://josecret.github.io',
-	base: '/edwtdocs',
+  site: 'https://josecret.github.io',
+  base: '/edwtdocs',
 
-	integrations: [
-		starlight({
-			title: 'EDWT Docs',
-			social: {
-				github: 'https://github.com/joSecret/edwtdocs',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'EDWT Docs',
+      social: {
+        github: 'https://github.com/joSecret/edwtdocs',
+      },
+      logo: {
+        src: './src/assets/logo.svg',
+      },
+      customCss: [
+        './src/styles/custom.css',
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: 'Example Guide', link: '/guides/example/' },
+          // ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
 });
---
+++ src/content/docs/guides/Install.md
@@ -0,0 +1,10 @@
+---
+title: Install
+description: EDWT install theme.
+---
+
+How to install drupal theme.
+
+## Installation theme
+
+- Read [about how-to install](https://www.drupal.org/docs/user_guide/en/extend-theme-install.html)
